0f09f9e589a36454106cbb3c787fdc74d6bab0ff
PeterManda19 <peter.pmanda.manda@gmail.com>
2022-09-08 11:51:00 -0700

    Betty documentation style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,10 @@
 #include <unistd.h>
 
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
