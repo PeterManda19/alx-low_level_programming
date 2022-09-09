a961d119c0fa30101a6f1ecc62597c0acda660d8
PeterManda19 <peter.pmanda.manda@gmail.com>
2022-09-09 10:58:46 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,6 +9,7 @@
 int main(void)
 {
 	int n, s;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* my code begins here */
@@ -24,6 +25,6 @@ int main(void)
 	else if (s < 6)	
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, s);
-    	}
+	}
 	return (0);
-}	
+}
