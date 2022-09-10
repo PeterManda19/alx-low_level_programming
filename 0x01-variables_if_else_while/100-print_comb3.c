d750b482aa28c9e8539419f8380ea82ef753797d
PeterManda19 <peter.pmanda.manda@gmail.com>
2022-09-09 18:39:14 -0700

    print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			if (i == j || j <= i)
+			{
+			}
+			else
+			{
+				putchar(x);
+				putchar(y);
+				if (x == 56 && y == 57)
+				{
+				}
+				else
+				{
+					putchar(44);
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
