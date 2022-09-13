4e796bbbc67b77cdf9cf6727bf002c401ec91abd
PeterManda19 <peter.pmanda.manda@gmail.com>
2022-09-13 10:20:02 -0700

    prints the first 98 Fibonacci numbers, starting with 1 and 2

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+ * main - first 98 Fibonacci numbers starting from 1
+ *
+ * Return: (0)
+ */
+
+int main(void)
+{
+	long int i;
+	long int prev = 1;
+	long int post = 2;
+	long int constraint = 10000000;
+	
+	long int prev1;
+	long int prev2;
+	long int post1;
+	long int post2;
+
+	printf("%lu", prev);
+
+	for (i = 1; i < 91; i++)
+	{
+		printf(",%lu", post);
+		post += prev;
+		prev = post - prev;
+	}
+	prev1 = (prev / constraint);
+	prev2 = (prev % constraint);
+	post1 = (post / constraint);
+	post2 = (post % constraint);
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", post1 + (post2 / constraint));
+		printf("%lu", post2 % constraint);
+		post1 = post1 + prev1;
+		prev1 = post1 - prev1;
+		post2 = post2 + prev2;
+		prev2 = post2 - prev2;
+	}
+	printf("\n");
+	return (0);
+}
