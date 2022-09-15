700122016ffbc344d40a0fca630dc192641fbf1d
PeterManda19 <peter.pmanda.manda@gmail.com>
2022-09-15 14:54:19 -0700

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * _isdigit - Check if paramter c represents an digit.
+ * _isdigit - Checks if paramter c is digit.
  * @c: Int representing an ascii character
  * Return: Return 1 if it is a digit otherwise return 0.
  */
