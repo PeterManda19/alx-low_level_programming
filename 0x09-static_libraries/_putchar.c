c7babe80b27d92524a2f9c0f8c210274f72fa467
PeterManda19 <peter.pmanda.manda@gmail.com>
2022-09-30 13:51:33 -0700

    static library c files

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,1 +1,12 @@
+#include "main.h"
 
+/**
+ * _isupper - check for uppercase characters
+ * @c: is an integer param
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
