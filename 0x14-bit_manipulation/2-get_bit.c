4bdd722c4019f2db2c7fe1030b3df56b2490a5bf
PeterManda19 <peter.pmanda.manda@gmail.com>
2022-10-27 21:23:21 +0200

    return bit in get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,7 +4,7 @@
  * get_bit - value of a bit at specified index
  * @n: decimal parameter
  * @index: index
- * Return: val
+ * Return: bit
  */
 
 int get_bit(unsigned long int n, unsigned int index)
@@ -12,7 +12,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	int bit;
 
 	bit = (n >> index);
-	
+
 	if (index > 32)
 	return (-1);
 	return (bit & 1);
